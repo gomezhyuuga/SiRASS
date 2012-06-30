5729170117c135954d70cab48e01fc79fd2b5ec5
Fernando Gómez <gomezhyuuga@me.com>
2012-06-30 04:49:05 -0500

    Agregado favicon

--- SiRASS/src/main/java/skyforge/sirass/form/Form.java
+++ SiRASS/src/main/java/skyforge/sirass/form/Form.java
@@ -12,11 +12,22 @@ public abstract class Form {
     Map<String, String[]> vars;
     SimpleDateFormat dateFormat;
 
+    /**
+     * Constructor básico de un form
+     *
+     * @param vars Map de clave-valor
+     */
     public Form(Map<String, String[]> vars) {
         this.vars = vars;
-        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     }
 
+    /**
+     * Constructor de un form básico con soporte de fechas formateadas
+     * "dd/MM/yyyy"
+     *
+     * @param vars Map de clave-valor
+     * @param dateFormat True para el soporte del dateFormat
+     */
     public Form(Map<String, String[]> vars, boolean dateFormat) {
         this.vars = vars;
         if (dateFormat) {
